b4be1decd2b75865d3b34390b1c778059dbb26fc
Igor Artus <igorengenheirodesoftware@gmail.com>
2024-12-13 13:47:12 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,26 @@
 "use client";
 
-import { Footer } from "@/components/Footer";
-import { Header } from "@/components/Header";
-import { PostList } from "@/components/PostList";
-import { PostProvider } from "@/contexts/PostContext";
+import { Button } from "@/components/Button";
+import { Container } from "@/components/Container";
+import { ThemeSwitch } from "@/components/ThemeSwitch";
+import { ThemeProvider } from "@/contexts/ThemeContext";
 
 const Page = () => {
-  
   return (
-      <div className="bg-gradient-to-r from-sky-500 to-purple-500 min-h-screen">
-        <PostProvider >
-        <div className="container mx-auto ">
-          <Header />
-          <PostList />
-          <Footer />
-        
-        </div>
-        </PostProvider>
-      </div>
+    <ThemeProvider>
+      <Container>
+        <header className="py-5">
+          <h1 className="text-3xl">Título de exemplo</h1>
+        </header>
+        <section>
+          <p className="my-5">Conteúdo de exemplo</p>
+
+          <Button label="Exemplo" onClick={() => { }} />
+          <ThemeSwitch />
+        </section>
+      </Container>
+    </ThemeProvider>
   );
-};
+}
 
 export default Page;
