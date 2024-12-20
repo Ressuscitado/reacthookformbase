95dc12ae614ec51dc65818b6083b679d0922b327
Igor Artus <igorengenheirodesoftware@gmail.com>
2024-12-20 16:52:01 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,21 @@
 "use client";
 
-import { Button } from "@/components/Button";
-import { Container } from "@/components/Container";
-import { ThemeSwitch } from "@/components/ThemeSwitch";
-import { ThemeProvider } from "@/contexts/ThemeContext";
+import { Chat } from "@/components/Chat";
+import { ChatProvider } from "@/contexts/ChatContext";
+import { UserProvider } from "@/contexts/UserContext";
 
 const Page = () => {
   return (
-    <ThemeProvider>
-      <Container>
-        <header className="py-5">
-          <h1 className="text-3xl">Título de exemplo</h1>
-        </header>
-        <section>
-          <p className="my-5">Conteúdo de exemplo</p>
+    <div className="container mx-auto max-w-lg px-2">
+      <UserProvider>
+        <ChatProvider>
+          <h1 className="text-3xl my-3 text-center">Chat simples</h1>
+          <Chat />
+        </ChatProvider>
+      </UserProvider>
 
-          <Button label="Exemplo" onClick={() => { }} />
-          <ThemeSwitch />
-        </section>
-      </Container>
-    </ThemeProvider>
+    </div>
   );
-}
+};
 
 export default Page;
